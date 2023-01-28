5e11acb90a43e7e956e8de5b744e45c6ec826edd
roham043 <66734575+roham043@users.noreply.github.com>
2023-01-28 10:11:40 +0330

    change header styles

--- src/Component/Header/Header.js
+++ src/Component/Header/Header.js
@@ -1,49 +1,47 @@
-import MoreBtn from './button/MoreBtn';
+import styles from './Header.module.css';
 import TicketBtn from './button/TicketBtn';
 import ResidenceBtn from './button/ResidenceBtn';
+import MoreBtn from './button/MoreBtn';
 import TourBtn from './button/TourBtn';
-
-
-
-import styles from './Header.module.css';
-
-import { ReactComponent as Alibaba } from '../../assets/icons/alibaba.svg'
-import { ReactComponent as Person } from '../../assets/icons/person.svg';
-import { ReactComponent as Receipt } from '../../assets/icons/receipt.svg';
-import { ReactComponent as Help } from '../../assets/icons/help.svg';
-
-
-
+import { BsFileEarmarkPerson } from "react-icons/bs";
+import { IoReceipt } from "react-icons/io5";
+import { MdOutlineLiveHelp } from "react-icons/md";
+import { ReactComponent as Alibaba } from '../../assets/icons/alibaba.svg';
+// import { ReactComponent as Person } from '../../assets/icons/person.svg';
+// import { ReactComponent as Receipt } from '../../assets/icons/receipt.svg';
+// import { ReactComponent as Help } from '../../assets/icons/help.svg';
 const Header = () => {
-  
     return (
         <div className={styles.container}>
             <div className={styles.logoContainer}>
                 <Alibaba className={styles.logoContainerAlibabaIcon} />
                 <div className={styles.logoContainerContent}>
-                    <h3>alibaba.ir</h3>
-                    <h6>خرید بلیط, هتل,تور</h6>
+                    <span >alibaba.ir</span>
+                    <span>خرید بلیط, هتل,تور</span>
                 </div>
 
             </div>
             <div className={styles.buttonContainer}>
                 <TicketBtn />
-                <ResidenceBtn />
+                <ResidenceBtn/>
                 <TourBtn/>
-                <MoreBtn />
+                <MoreBtn/>
             </div>
             <div className={styles.linkContainer}>
                 <div>
+                    <BsFileEarmarkPerson className={styles.personIcon}/>
                     <p>ورود یا ثبت نام</p>
-                    <Person />
+
                 </div>
                 <div>
+                    <IoReceipt  className={styles.ReceiptIcon}/>
                     <p>پیگیری خرید</p>
-                    <Receipt />
+
                 </div>
                 <div>
+                    <MdOutlineLiveHelp className={styles.HelpIcon}/>
                     <p>پشتیبانی آنلاین</p>
-                    <Help />
+
                 </div>
             </div>
         </div>
--- src/Component/Header/button/TicketBtn.js
+++ src/Component/Header/button/TicketBtn.js
@@ -11,7 +11,7 @@ import images from '../image';
 
 
 export default function TicketBtn() {
-  const array = [0,1,2,3,4,5,6]
+  
  
   const dispatch = useDispatch();
 
