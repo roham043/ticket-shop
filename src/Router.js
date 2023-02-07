25f53193af6443dfb273379ed7ea0b6f920d4799
roham043 <66734575+roham043@users.noreply.github.com>
2023-02-07 12:13:06 +0330

    change IdentificationBothTicket.js name to DomesticFlightPassengersPage

--- src/Router.js
+++ src/Router.js
@@ -3,7 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 import Header from './Component/Header/Header';
 import App from './App';
 import Ticket from './Component/pages/DomesticTicket/mainpage/Ticket';
-import IdentificationBothTicket from './Component/pages/DomesticTicket/mainpage/IdentificationBothTicket';
+import DomesticFlightPassengersPage from './Component/pages/DomesticTicket/mainpage/DomesticFlightPassengersPage';
 
 
 
@@ -13,7 +13,7 @@ class Router extends React.Component {
             <div>
                 <Header />
                 <Routes>
-                    <Route exact path="/IdentificationBothTicket" element={<IdentificationBothTicket />} />
+                    <Route exact path="/IdentificationBothTicket" element={<DomesticFlightPassengersPage />} />
                     <Route exact path="/" element={<App />} />
                     <Route path="ticket" element={<Ticket />} />
 
