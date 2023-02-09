0134de230ff4dc3c70577e41ea673b723bdfe8ab
roham043 <66734575+roham043@users.noreply.github.com>
2023-02-09 18:46:04 +0330

    created styles of ticket information DomesticFlightConfirmPage.jsx

--- src/Component/pages/DomesticTicket/mainpage/DomesticFlightConfirmPage.jsx
+++ src/Component/pages/DomesticTicket/mainpage/DomesticFlightConfirmPage.jsx
@@ -1,5 +1,7 @@
 import styles from './confirmPageStyles.module.css';
 import ConfirmpageStepper from '../child/Steppers/ConfirmpageStepper';
+import { HiTicket } from "react-icons/hi2";
+
 
 const DomesticFlightConfirmPage = () =>{
     return(
@@ -7,7 +9,16 @@ const DomesticFlightConfirmPage = () =>{
             <div className={styles.stepper}>
                   <ConfirmpageStepper/>
             </div>
-            <div className={styles.ticketInformation}></div>
+            <div className={styles.ticketInformation}>
+                <div className={styles.ticketInformationHeader}>
+                        <HiTicket className={styles.ticketInformationIcon}/>
+                        <h3 style={{fontFamily:'iranyekan'}}>اطلاعات بلیط</h3>
+                </div>
+                <div className={styles.ticketInformationFooter}>
+                       <div className={styles.ticketWent}></div>
+                       <div className={styles.returnTicket}></div>
+                </div>
+            </div>
             <div className={styles.specificationOfPassengers}></div>
             <div className={styles.travelMessage}></div>
             <div className={styles.discountCode}></div>
